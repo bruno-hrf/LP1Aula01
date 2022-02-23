0b9f60afd5da40e84bc478776fae21ac31490f5d
Bruno Rodrigues <rodrigues_bruno@outlook.pt>
2022-02-23 22:59:59 +0000

    Normal Strings and Verbatim

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    /// <summary>
+    /// This program prints "Hello, World!" 
+    /// then prints"Bye World!"
+    /// </summary>
+    
+        class Program
+    {
+        static void Main (string[] args)
+        {
+            string a = "Uma Plica \u0027";
+            string b = "Uma nova linha \nlinha";
+            string c = @"Uma Plica 0x0027 em Unicode";
+            string d = @"Literalmente um \n";
+
+            Console.WriteLine(a +"\n"+ b +"\n"+ c + "\n" + d);
+            
+        }
+
+    }
+}
