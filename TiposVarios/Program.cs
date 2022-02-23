8316990aafd124f3fd1736b6448b50199c64a087
Bruno Rodrigues <rodrigues_bruno@outlook.pt>
2022-02-23 21:37:02 +0000

    Integer types

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int x = 10;         //inteiro decimal
+            int y = 0b1010;     //inteiro binário
+            int z = 0xA;        //inteiro hexadecimal
+
+            Console.WriteLine(x +" " + y +" "+ z);
+
+        }
+
+    }
+}
