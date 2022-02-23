c489b74dfe8bcf87393db835e63bf6314e72a945
Bruno Rodrigues <rodrigues_bruno@outlook.pt>
2022-02-23 23:37:39 +0000

    Added more strings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -2,10 +2,7 @@
 
 namespace VariasStrings
 {
-    /// <summary>
-    /// This program prints "Hello, World!" 
-    /// then prints"Bye World!"
-    /// </summary>
+    
     
         class Program
     {
@@ -16,7 +13,23 @@ namespace VariasStrings
             string c = @"Uma Plica 0x0027 em Unicode";
             string d = @"Literalmente um \n";
 
+            string x = "String "+ "conc" + "atizada";
+            string xy = x + "e com texto adicionado!";
+
+            int docesCompra = 3;
+            int docesComi =2;
+            string y = $"comprei {docesCompra} doces e comi {docesComi}";
+
+            int salario = 500;
+            int banco = 2300; 
+            string z = $"recebi {salario}€ agora tenho {banco + salario}€";
+
+            string last = String.Format("X = {1} e Y = {0}", salario, banco);
+
             Console.WriteLine(a +"\n"+ b +"\n"+ c + "\n" + d);
+            Console.WriteLine(x +"\n"+ xy +"\n"+ y + "\n" + z + "\n" + last);
+            
+            Console.WriteLine(@"Sálario = {0}€", salario);
             
         }
 
