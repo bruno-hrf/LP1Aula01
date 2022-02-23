cdf58374a84cac672717208008bc863683fcc73a
Bruno Rodrigues <rodrigues_bruno@outlook.pt>
2022-02-23 21:14:07 +0000

    Add Comments

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Bye World!");
+﻿using System;
+
+namespace OlaMundo
+{
+    /// <summary>
+    /// This program prints "Hello, World!" 
+    /// then prints"Bye World!"
+    /// </summary>
+    
+        class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bye World!");
+        }
+
+    }
+}
