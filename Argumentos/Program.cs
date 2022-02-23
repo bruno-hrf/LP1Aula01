6224f76998e6198150fda5c359ee1ffe2a1b227b
Bruno Rodrigues <rodrigues_bruno@outlook.pt>
2022-02-23 21:02:45 +0000

    Print arg

---
+++ Argumentos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int i= 0;
+            for (i = 0; i < args.Length; ++i)
+            {
+                Console.WriteLine(args[i]);
+            }
+
+        }
+
+    }
+}
