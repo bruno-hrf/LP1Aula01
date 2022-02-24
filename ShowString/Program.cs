1dcecacbf0092fdb234837c4f160f8e1dad63ebc
Bruno Fernandes <a22103346@alunos.ulht.pt>
2022-02-24 15:38:32 +0100

    Returns String

---
+++ ShowString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ShowString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string frase;
+
+            Console.WriteLine("Escreva uma frase: ");
+            frase = Console.ReadLine();
+            Console.WriteLine("A sua frase foi :" +frase);
+        }
+    }
+}
